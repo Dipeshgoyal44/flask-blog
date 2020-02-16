8b3a766bc9f9687e515d7dcca2c6bff5735a573e
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-02-17 10:58:05 +1100

    comments

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
+#Imports                    
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email,EqualTo
 
+#Registration Form
 class RegistrationForm(FlaskForm):
     username= StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email= StringField('Email', validators=[DataRequired(), Email()])
@@ -9,6 +11,7 @@ class RegistrationForm(FlaskForm):
     confirm_password= PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit=SubmitField('Sign Up')
 
+#Login Form
 class LoginForm(FlaskForm):
     username= StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password= PasswordField('Password', validators=[DataRequired()])
