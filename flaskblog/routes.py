ed8193eab62a6839d000a29addaa52c1a7053650
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-02-29 22:03:54 +1100

    register

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import  render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-
+db.create_all()
 
 #Dummy Data
 posts = [
