ab82b18dc8d1cfe95c563f75696573d155b76c51
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-02-29 22:18:32 +1100

    user login

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 app = Flask(__name__)
 
 #Hash Security Key
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = '44ad1670c8d8186ca39190bb09f6a781'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager= LoginManager(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,6 +2,7 @@ from flask import  render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
+from flask_login import login_user
 db.create_all()
 
 #Dummy Data
@@ -50,8 +51,9 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.username.data == 'admin' and form.password.data =='password':
-            flash('You Have Been Logged In!','success')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful!. Please check your login details.', 'danger')
