857da25f5705eeb905be13b2ce01a836b6f9af58
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-07-03 22:58:30 +1000

    routes

---
+++ flaskblog/main/routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+
+main = Blueprint('main',__name__)
+
+
+#Routes
+@main.route('/')
+@main.route('/home')
+def home():
+    #PAGINATION
+    page = request.args.get('page',1, type = int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=5) #No. Of Posts Per Page
+    return render_template('home.html', posts=posts)
+
+#About
+@main.route('/about')
+def about():
+    return render_template('about.html', title='About')  
+
+
